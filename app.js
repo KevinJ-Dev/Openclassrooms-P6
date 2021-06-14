7a5a8975842ea65a5aa0ffcfc972318fe5e616e7
Kevin <jdev.kevin@gmail.com>
2021-06-14 12:01:30 +0200

    Add models, requetes CORS, BodyParser, img, routes

--- app.js
+++ app.js
@@ -20,10 +20,29 @@ mongoose.connect('mongodb+srv://Admin:tX56lH0tchRbbTTk@cluster0.pi3xb.mongodb.ne
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+// Models
 const User = require('./models/user');
+
 
 // Routes 
 const userRoutes = require('./routes/user');
+
+// Donner accée aux requêtes et éviter les erreurs CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+// Traitement par BodyParser
+app.use(bodyParser.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// Url des routes
+
+app.use('/', userRoutes);
 
 //exporter une application pour rentrer dans les autres fichiers du projet notament le serveur
 module.exports = app;
